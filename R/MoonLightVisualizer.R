6a44cb597541b155bd96c29f4c76a4072c0603f3
CDarling25 <135234079+CDarling25@users.noreply.github.com>
2025-04-27 16:39:23 -0400

    started function and added data to data-raw

---
+++ R/MoonLightVisualizer.R
@@ -0,0 +1,16 @@
+#' Visual Analysis of moon phase and light sensor data
+#'
+#' @details This function takes in a desired start date and will return a visualization for a week's worth of data
+#'
+#' @author Cameron Darling
+#' @export
+#' @import dplyr
+#' @import ggplot2
+#' @param start_date The desired start date of visual analysis
+#' @param ... Additional arguments
+#' @return returns a graph and displays it
+#' @examples
+#' moon_light_visual("")
+moon_light_visual <- function(start_date, ...) {
+
+}
---
+++ data-raw/moon_light.R
@@ -0,0 +1,2 @@
+moon_light <- read.csv("data-raw/SQM_MacLeish_2025-03.csv")
+usethis::use_data(moon_light, overwrite = TRUE)
